d9b2bd89d486e0b03269a8b0f94b60ca026c21e4
Christian Weiske <cweiske@cweiske.de>
2019-11-12 16:50:23 +0100

    Better JSON schema validation error messages

--- bin/validate-game.sh
+++ bin/validate-game.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Validate an OUYA game schema file with several validators
 set -e
-jsonschema -i "$1" ouya-game.schema.json
 validate-json "$1" ouya-game.schema.json
+jsonschema -i "$1" ouya-game.schema.json
