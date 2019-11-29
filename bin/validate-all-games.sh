0628d46b1b2943258eb3a76ea3d72e9ae748662f
Christian Weiske <cweiske@cweiske.de>
2019-11-29 21:04:11 +0100

    Make all-game validation run much faster
    .. by running the javascript validation tool only once.
    
    On my machine, I am down from 3m9s to 42s

--- bin/validate-all-games.sh
+++ bin/validate-all-games.sh
@@ -3,8 +3,11 @@
 # in all the folders listed in the folders file
 set -e
 for folder in `cat folders`; do
+    iparams=""
     for gamefile in $folder/*.json; do
+        iparams="$iparams -i $gamefile"
         echo "$gamefile"
-        ./bin/validate-game.sh "$gamefile"
+        validate-json "$gamefile" ouya-game.schema.json
     done
+    jsonschema $iparams ouya-game.schema.json
 done
