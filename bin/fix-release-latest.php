048ed2e6f43069450a1237bb166865393280ba8d
Christian Weiske <cweiske@cweiske.de>
2020-02-05 19:57:58 +0100

    Scripts to remove FIXME releases and set the "latest" release property

---
+++ bin/fix-release-latest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Set the "latest" property correctly if there are multiple releases.
+ */
+array_shift($argv);
+$files = $argv;
+if (count($files) == 0) {
+    echo "Pass some game json files\n";
+    exit(1);
+}
+
+foreach ($files as $file) {
+    $game = json_decode(file_get_contents($file));
+    echo $game->packageName . "\n";
+
+    $modified = false;
+    $highestCode = 0;
+    $highestKey  = null;
+    foreach ($game->releases as $key => $release) {
+        if (version_compare($release->versionCode, $highestCode, '>')) {
+            $highestCode = $release->versionCode;
+            $highestKey  = $key;
+        }
+    }
+    foreach ($game->releases as $key => $release) {
+        $highest = $highestKey == $key;
+        if ($release->latest == $highest) {
+            continue;
+        }
+        $release->latest = $highest;
+        $modified = true;
+    }
+
+    if ($modified) {
+        echo " updating file\n";
+        file_put_contents(
+            $file,
+            json_encode($game, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . "\n"
+        );        
+    }
+}
+?>
---
+++ bin/remove-fixme-releases.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Remove releases with "FIXME" download URLs
+ */
+array_shift($argv);
+$files = $argv;
+if (count($files) == 0) {
+    echo "Pass some game json files\n";
+    exit(1);
+}
+
+foreach ($files as $file) {
+    $game = json_decode(file_get_contents($file));
+    echo $game->packageName . "\n";
+
+    $unset = [];
+    foreach ($game->releases as $key => $release) {
+        if ($release->url !== 'FIXME') {
+            echo " already ok for version " . $release->name . "\n";
+            continue;
+        }
+
+        echo " URL is FIXME for " . $release->name . "\n";
+        $unset[] = $key;
+    }
+
+    if (count($unset)) {
+        if (count($unset) == count($game->releases)) {
+            echo " NO releases anymore - skipping!\n";
+            continue;
+        }
+        foreach ($unset as $key) {
+            unset($game->releases[$key]);
+        }
+        $game->releases = array_values($game->releases);//re-key
+        echo " updating file\n";
+        file_put_contents(
+            $file,
+            json_encode($game, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . "\n"
+        );        
+    }
+}
+?>
