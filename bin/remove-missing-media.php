f362d9cb4f9f8b031a5fb0eb39650622d2438543
Christian Weiske <cweiske@cweiske.de>
2020-02-05 20:38:39 +0100

    Script to remove unrecoverable cloudfront and filepicker images from game files

---
+++ bin/remove-missing-media.php
@@ -0,0 +1,44 @@
+#!/usr/bin/env php
+<?php
+/**
+ * Walk through json game files and remove all media images
+ * that link to cloudfront or filepicker.
+ */
+array_shift($argv);
+
+$files = $argv;
+if (count($files) == 0) {
+    echo "Pass some game json files\n";
+    exit(1);
+}
+
+function isMissingUrl($url)
+{
+    return strpos($url, '.cloudfront.net/') !== false
+        || strpos($url, 'www.filepicker.io/api') !== false;
+}
+
+foreach ($files as $file) {
+    $game = json_decode(file_get_contents($file));
+    echo $game->packageName . "\n";
+
+    $unset = [];
+    foreach ($game->media as $key => $medium) {
+        if (isMissingUrl($medium->url)) {
+            $unset[] = $key;
+        }
+    }
+    if (count($unset) == 0) {
+        continue;
+    }
+    foreach ($unset as $key) {
+        unset($game->media[$key]);
+    }
+    $game->media = array_values($game->media);//re-key
+    file_put_contents(
+        $file,
+        json_encode($game, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . "\n"
+    );
+    echo " written\n";
+}
+?>
