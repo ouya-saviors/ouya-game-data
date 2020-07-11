418e0dcb205d574d035942daa9dc31330fb139aa
DeviRich <1254758-devirich@users.noreply.gitlab.com>
2020-07-11 08:17:27 -0600

    update validate-all-games
    Enable validate to first run on recently committed games

--- bin/validate-all-games.sh
+++ bin/validate-all-games.sh
@@ -2,6 +2,12 @@
 # Validate all ouya game data files
 # in all the folders listed in the folders file
 set -e
+for gamefile in `git diff --name-only HEAD~1 | grep -E "^(\`paste -d '|' -s folders\`)"`; do
+    iparams="$iparams -i $gamefile"
+    echo "$gamefile"
+    validate-json "$gamefile" ouya-game.schema.json
+done
+
 for folder in `cat folders`; do
     iparams=""
     for gamefile in $folder/*.json; do
