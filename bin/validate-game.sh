d4dcff33586fa5f9c3c7779504be50f54fbf3c2d
Christian Weiske <cweiske@cweiske.de>
2021-04-14 21:23:06 +0200

    Check if filename has been passed to validate-game.sh

--- bin/validate-game.sh
+++ bin/validate-game.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 # Validate an OUYA game schema file with several validators
 set -e
+
+if [ $# -lt 1 ]; then
+    echo Error: game json filename missing
+    exit 1
+fi
+
+if [ ! -f "$1" ]; then
+    echo Error: file does not exist
+    exit 2
+fi
+
 validate-json "$1" ouya-game.schema.json
 jsonschema -i "$1" ouya-game.schema.json
